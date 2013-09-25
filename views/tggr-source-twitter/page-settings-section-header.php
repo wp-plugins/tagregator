7453308473f7d9e79f5e6c72063d261356f48b30
iandunn <iandunn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-25 18:26:15 +0000

    Add link to detailed instructions for creating Twitter app.
    git-svn-id: https://plugins.svn.wordpress.org/tagregator/trunk@778503 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/tggr-source-twitter/page-settings-section-header.php
+++ views/tggr-source-twitter/page-settings-section-header.php
@@ -1,1 +1,1 @@
-<p>You can obtain the settings below by creating an account at <a href="https://dev.twitter.com/">Twitter's developer portal</a>, and then creating a new application.</p>
+<p>You can obtain the settings below by <a href="https://dev.twitter.com/apps/new">creating a new application</a> at Twitter's developer portal. For detailed instructions, please see <a href="http://wordpress.org/plugins/tagregator/installation/">the Installation page</a>.</p>
